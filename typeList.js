8b29dcea7c4828d8aeda99df1f48f723cd274a78
MestrieEsetabn <esteban94.em@gmail.com>
2020-02-29 21:29:02 +0100

    ok

--- typeList.js
+++ typeList.js
@@ -3,181 +3,181 @@ module.exports = {
         backgroundColor: "#A8A77A",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     fire: {
         backgroundColor: "#EE8130",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     water: {
         backgroundColor: "#6390F0",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     electric: {
         backgroundColor: "#F7D02C",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     grass: {
         backgroundColor: "#7AC74C",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     ice: {
         backgroundColor: "#96D9D6",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     fighting: {
         backgroundColor: "#C22E28",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     poison: {
         backgroundColor: "#A33EA1",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     ground: {
         backgroundColor: "#E2BF65",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     flying: {
         backgroundColor: "#A98FF3",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     psychic: {
         backgroundColor: "#F95587",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     bug: {
         backgroundColor: "#A6B91A",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     rock: {
         backgroundColor: "#B6A136",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     ghost: {
         backgroundColor: "#735797",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     dragon: {
         backgroundColor: "#6F35FC",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     dark: {
         backgroundColor: "#705746",
@@ -187,24 +187,24 @@ module.exports = {
         backgroundColor: "#B7B7CE",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     },
     fairy: {
         backgroundColor: "#D685AD",
         fontSize: 20,
         width: "50%",
-
         color: "white",
         borderWidth: 0.5,
         bordercolor: "white",
         alignContent: "center",
         textAlign: "center",
-        marginBottom: 10
+        marginBottom: 10,
+        fontWeight: "bold"
     }
 };
